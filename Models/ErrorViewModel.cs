bdfe56cb8fc1ef732ba49bc5fb9100161f94862a
dima-source <udovenkodima7@gmail.com>
2023-11-03 13:07:25 +0200

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Folder_hierarchy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
